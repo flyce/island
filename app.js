cbb4e1e50053a98963b5e155e81cde461f0894fb
Echo <mail@flyce.cn>
2019-11-28 22:51:34 +0800

    路由自动加载 && 入口文件封装成类

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const Koa = require('koa')
-const classic = require('./api/v1/classic')
-const book = require('./api/v1/book')
+
+const InitManager = require('./core/init')
 
 const app = new Koa()
 
-app.use(book.routes())
-app.use(classic.routes())
+InitManager.initCore(app)
+process.cwd()
 
 app.listen(3000, () => {
     console.log('koa running on port 3000')
